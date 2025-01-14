0ff6616bb967ef90c4e80f6ee180a1db64b1fef4
princemashumu <princemashumu@yahoo.com>
2025-01-14 20:59:23 +0200

    feat. added activities

--- server/controllers/activitiesController.js
+++ server/controllers/activitiesController.js
@@ -3,6 +3,15 @@ const activities = {
     rainy: ["Museum Visit", "Board Games", "Cooking Class"],
     cloudy: ["Shopping", "Photography", "Cafe Hopping"],
   };
+  // /* { date: '2025-01-14', day: { condition: { text: 'Snow', icon: 'snow' }, avgtemp_c: 15 } },
+  //   { date: '2025-01-15', day: { condition: { text: 'Thunderstorm', icon: 'thunderstorms-night-rain' }, avgtemp_c: 18 } },
+  //   { date: '2025-01-16', day: { condition: { text: 'Clear', icon: 'clear-day' }, avgtemp_c: 12 } },
+  //   { date: '2025-01-17', day: { condition: { text: 'Partly Cloudy', icon: 'partly-cloudy-day' }, avgtemp_c: 14 } },
+  //   { date: '2025-01-18', day: { condition: { text: 'Rain', icon: 'rain' }, avgtemp_c: 13 } },
+  //   { date: '2025-01-19', day: { condition: { text: 'Fog', icon: 'fog' }, avgtemp_c: 11 } },
+  //   { date: '2025-01-20', day: { condition: { text: 'Snow', icon: 'snow' }, avgtemp_c: 10 } },
+  //  */
+
   
   const getActivities = (req, res) => {
     const { condition } = req.query;
