50cf353cc281ce2fafd0056f603d77ac72af0c89
Thapelo Somo <edwardthapelo55@gmail.com>
2025-01-15 12:23:09 +0200

    deleted the activity.js

--- server/routes/activities.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getActivities } = require("../controllers/activitiesController");
-
-const router = express.Router();
-
-router.get("/", getActivities);
-
-module.exports = router;
